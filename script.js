3595937e80ff5d0137e31d548bf74386e85eef2d
ayfabio <127374141+AyFabio@users.noreply.github.com>
2023-06-26 02:51:15 -0400

    Organized code with functions and comments

--- script.js
+++ script.js
@@ -1,72 +1,122 @@
 let firstNum;
 let operator;
-let lastNum;
+let secondNum;
+let equalPressed = false;
+const buttons = document.querySelectorAll('button');
+const currentText = document.querySelector(".current-text");
+const prevText = document.querySelector(".prev-text");
+const decimal = document.querySelector('.decimal');
+const digit = document.querySelectorAll('.digit');
 
-function add(a, b) {
-    return a + b;
+buttons.forEach(button => button.addEventListener('click', useCalc));
+
+function useCalc(e) {
+    btn = this.textContent;
+    switch (btn) {
+        case 'Clear':
+            clearDisplay();
+            break;
+
+        case 'Del':
+            deleteInput();
+            break;
+    }
+    //if divided by 0, must clear.
+    if (currentText.textContent === 'Very funny. Press Clear.') return;
+    //if button is number, decimal, or operator, compute display
+    if (btn !== 'Clear' && btn !== 'Del') {
+        appendDisplay(btn);
+    }
+    
+    
 }
 
-function subtract(a, b) {
-    return a - b;
+//deletes the most recent value added
+function deleteInput() {    
+    currentText.textContent = currentText.textContent.slice(0, currentText.textContent.length -1);
+    checkRules();
 }
 
-function multiply(a, b) {
-    return a * b;
+// Clears display and resets all values
+function clearDisplay() {
+    firstNum = '';
+    operator = '';
+    secondNum = '';
+    currentText.textContent = '';
+    prevText.textContent = '';
+    equalPressed = false;
+    checkRules();
 }
 
-function divide(a, b) {
-    return a / b;
-}
-
-function operate(operator, firstNum, lastNum) { 
-    switch (operator) {
-        case "+":
-            return add(firstNum, lastNum);
-            
-        case "-":
-            return subtract(firstNum, lastNum);
-
-         case "x":
-            return multiply(firstNum, lastNum);
-
-          case "/":
-            return divide(firstNum, lastNum);
+// compute what to do on display
+function appendDisplay(btn) {
+    //if number or decimal, add to display
+    if (!isNaN(Number(btn)) || btn === '.') {
+        currentText.textContent += btn;
+        checkRules();
+    } else if (btn === "=") {
+        //if equals is pressed, allows pressing equals again to run same equation using new number. if screen blank, do nothing.
+        if (!operator || !currentText.textContent) return; 
+        if (equalPressed === true) {
+            prevText.textContent = currentText.textContent + ' ' + operator + ' ' + secondNum;
+            firstNum = Number(currentText.textContent);
+            currentText.textContent = Number(operate(operator, firstNum,secondNum).toFixed(3));
+        } else {
+            secondNum = Number(currentText.textContent);
+            prevText.textContent += ' ' + currentText.textContent;
+            currentText.textContent = Number(operate(operator, firstNum,secondNum).toFixed(3));
+            equalPressed = true;
+        }
+        //if operator pressed, allow math off of an equaled number, or if operator already present, allow math off previously entered numbers. 
+    } else {
+        if (currentText.textContent) {
+            if (equalPressed) {
+                firstNum = Number(currentText.textContent);
+                prevText.textContent = firstNum + ' ' + btn;
+            } else if (operator) {
+                secondNum = Number(currentText.textContent);
+                firstNum = Number(operate(operator, firstNum, secondNum));
+                prevText.textContent = firstNum + ' ' + btn;
+            } else {
+                prevText.textContent = currentText.textContent + ' ' + btn;
+                firstNum = Number(currentText.textContent);
+            }
+            currentText.textContent = '';
+            operator = btn;
+            equalPressed = false;
+            checkRules();
+        } else {
+            prevText.textContent = firstNum + ' ' + btn;
+            operator = btn;
+            checkRules();
+        }
     }
 }
- function populateDisplay () {
-    const buttons = document.querySelectorAll('button');
-    const currentText = document.querySelector(".current-text");
-    const prevText = document.querySelector(".prev-text");
 
-    buttons.forEach((btn) => {
-        btn.addEventListener('click', () => {
-            if (btn.textContent === "x" || btn.textContent === "/" || btn.textContent === "+" || btn.textContent === "-") {
-                if (operator) {
-                    lastNum = +currentText.textContent;
-                currentText.textContent = Number(operate(operator, firstNum, lastNum).toFixed(3));
-                }
-                prevText.textContent = Number(Number(currentText.textContent).toFixed(3)) + ' ' + btn.textContent;
-                firstNum = +currentText.textContent;
-                operator = btn.textContent;
-                currentText.textContent = '';
-            } else if (btn.textContent === '='){
-                lastNum = +currentText.textContent;
-                prevText.textContent += ' ' + currentText.textContent + " =";
-                currentText.textContent = Number(operate(operator, firstNum, lastNum).toFixed(3));
-                operator = undefined;
-            } else if (btn.textContent === 'Clear') {
-                currentText.textContent = '';
-                prevText.textContent = '';
-                firstNum = undefined;
-                lastNum = undefined;
-                operator = undefined; 
-            } else if (btn.textContent === 'Del') {
-                currentText.textContent = currentText.textContent.slice(0, currentText.textContent.length -1);
-            } else {
-            currentText.textContent += +btn.textContent;
+// disables decimal if one is already added, disables input if too long.
+function checkRules() {
+    decAdded = Number(currentText.textContent.includes('.'));
+    decAdded ? decimal.disabled = true : decimal.disabled = false;
+
+    currentText.textContent.length > 14 ? digit.forEach(digit => digit.disabled = true) : digit.forEach(digit => digit.disabled = false);
+}
+
+function operate(operator, firstNum, secondNum) {
+    switch (operator) {
+        case "+":
+            return firstNum + secondNum;
+            
+        case "-":
+            return firstNum - secondNum;
+
+         case "x":
+            return firstNum * secondNum;
+
+          case "/":
+            if (secondNum === 0) {
+                currentText.textContent = 'Very funny. Press Clear.';
+                return; 
             }
-        });
-    });
- }
-
- populateDisplay ()
+            return firstNum / secondNum;
+    }
+}
